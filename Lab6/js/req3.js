59e93105324e0a379df65f99c64b4aa1acfb6b19
HoangKhangKhuPho4 <22130116@st.hcmuaf.edu.vn>
2024-11-04 14:51:13 +0700

    update lab6 Implement

--- Lab6/js/req3.js
+++
@@ -1,6 +0,0 @@
-   // Xử lý sự kiện click vào footer để điều hướng tới trang req4.html
-   document
-   .getElementById("starterCodeLink")
-   .addEventListener("click", function () {
-     window.location.href = "req4.html";
-   });
